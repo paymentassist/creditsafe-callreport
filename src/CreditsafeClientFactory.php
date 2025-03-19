1245894a03cd3c3463c097093f18214753745540
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2025-03-19 12:01:07 +0000

    update client factory

--- src/CreditsafeClientFactory.php
+++ src/CreditsafeClientFactory.php
@@ -2,27 +2,33 @@
 
 namespace PaymentAssist;
 
+use Http\Adapter\Guzzle7\Client;
+use Http\Client\Common\PluginClient;
 use Phpro\SoapClient\Caller\EngineCaller;
 use Phpro\SoapClient\Caller\EventDispatchingCaller;
+use Phpro\SoapClient\Soap\DefaultEngineFactory;
+use Soap\ExtSoapEngine\ExtSoapOptions;
+use Soap\Psr18Transport\Psr18Transport;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
-use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
 
 class CreditsafeClientFactory
 {
-
-    public static function factory(string $wsdl) : \PaymentAssist\CreditsafeClient
+    public static function factory(string $wsdl) : CreditsafeClient
     {
-        $engine = ExtSoapEngineFactory::fromOptions(
-            ExtSoapOptions::defaults($wsdl, [])
-                ->withClassMap(CreditsafeClassmap::getCollection())
+        $engine = DefaultEngineFactory::create(
+            ExtSoapOptions::defaults($wsdl)->withClassMap(CreditsafeClassmap::getCollection()),
+            Psr18Transport::createForClient(
+                new PluginClient(
+                    Client::createWithConfig(['headers' => ['User-Agent' => 'Payment Assist Client/1.0']]),
+                    [new CreditsafeMiddleware()]
+                )
+            )
         );
+
         $eventDispatcher = new EventDispatcher();
         $caller = new EventDispatchingCaller(new EngineCaller($engine), $eventDispatcher);
 
         return new CreditsafeClient($caller);
     }
-
-
 }
 
